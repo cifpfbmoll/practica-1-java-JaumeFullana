bbd6a44d44cee905973d0d008d4d94ee53ed1579
JaumeFullana <jfullana@cifpfbmoll.eu>
2021-01-19 17:14:08 +0100

    modificat posicio moduls

--- ACT1P1.java
+++ ACT1P1.java
@@ -4,6 +4,82 @@ public class ACT1P1 {
 
     static Scanner sc= new Scanner(System.in);
 
+    public static void main(String[] args) {
+        System.out.println("Bienvenido! escribe un numero del 1 al 9 para elegir uno de los diferentes modulos del programa. Escribe 0 para slir de este.");
+        int numero=sc.nextInt();
+        sc.nextLine();
+        while (numero!=0){
+            switch (numero) {
+                case 1:
+                leerNumeros();
+                break;
+
+                case 2:
+                LeerNumerosAlReves();
+                break;
+
+                case 3:
+                hacerMedias();
+                break;
+
+                case 4:
+                System.out.println("Escribe una palabra o frase.");
+                String texto4=sc.nextLine();
+                int numeroCaracteres=contarCaracteres(texto4);
+                System.out.println("El texto, sin contar espacios, contiene "+numeroCaracteres+" caracteres.");
+                break;
+
+                case 5:
+                System.out.println("Escribe una palabra o frase.");
+                String texto5=sc.nextLine();
+                String textoGirado=girarTexto(texto5);
+                System.out.println("El texto girado quedaria asi: "+textoGirado);
+                break;
+
+                case 6:
+                System.out.println("Escribe una palabra o frase.");
+                String texto6=sc.nextLine();
+                String textoSinEspacios=quitarEspacios(texto6);
+                System.out.println("El texto sin espacios quedaria asi: "+textoSinEspacios);
+                break;
+
+                case 7:
+                System.out.println("Escribe una palabra o frase.");
+                String texto7a=sc.nextLine();
+                System.out.println("Escribe otra palabra o frase.");
+                String texto7b=sc.nextLine();
+                String textoConcatenado=concatenarTexto(texto7a, texto7b);
+                System.out.println("Los dos textos concatenados quedan asi: "+textoConcatenado);
+                break;
+
+                case 8:
+                System.out.println("Escribe una palabra o frase.");
+                String texto8=sc.nextLine();
+                System.out.println("Escribe una vocal.");
+                String vocal=sc.nextLine();
+                cambiarVocales(texto8, vocal);
+                break;
+
+                case 9:
+                System.out.println("Escribe una palabra o frase.");
+                String texto9=sc.nextLine();
+                codigoCaracteres(texto9);
+                break;
+
+                case 0:
+                System.out.println("Vale! hasta la proxima");
+                break;
+
+                default:
+                System.out.println("ERROR: esa opcion no existe.");
+            }
+            System.out.println("");
+            sc=new Scanner(System.in);
+            System.out.println("Elige una nueva funcion(1-9) o sal del programa(0):");
+            numero=sc.nextInt();
+            sc.nextLine();
+        }
+    }
     public static void leerNumeros() {
         System.out.println("Bienvenido al modulo de leer numeros.");
         int [] listaNumeros= new int[5];
@@ -94,82 +170,6 @@ public class ACT1P1 {
     public static void codigoCaracteres(String texto) {
         for (int i=0;i<texto.length();i++){
             System.out.println("El codigo ASCII del caracter "+texto.charAt(i)+" es "+(int) texto.charAt(i));
-        }
-    }
-    public static void main(String[] args) {
-        System.out.println("Bienvenido! escribe un numero del 1 al 9 para elegir uno de los diferentes modulos del programa. Escribe 0 para slir de este.");
-        int numero=sc.nextInt();
-        sc.nextLine();
-        while (numero!=0){
-            switch (numero) {
-                case 1:
-                leerNumeros();
-                break;
-
-                case 2:
-                LeerNumerosAlReves();
-                break;
-
-                case 3:
-                hacerMedias();
-                break;
-
-                case 4:
-                System.out.println("Escribe una palabra o frase.");
-                String texto4=sc.nextLine();
-                int numeroCaracteres=contarCaracteres(texto4);
-                System.out.println("El texto, sin contar espacios, contiene "+numeroCaracteres+" caracteres.");
-                break;
-
-                case 5:
-                System.out.println("Escribe una palabra o frase.");
-                String texto5=sc.nextLine();
-                String textoGirado=girarTexto(texto5);
-                System.out.println("El texto girado quedaria asi: "+textoGirado);
-                break;
-
-                case 6:
-                System.out.println("Escribe una palabra o frase.");
-                String texto6=sc.nextLine();
-                String textoSinEspacios=quitarEspacios(texto6);
-                System.out.println("El texto sin espacios quedaria asi: "+textoSinEspacios);
-                break;
-
-                case 7:
-                System.out.println("Escribe una palabra o frase.");
-                String texto7a=sc.nextLine();
-                System.out.println("Escribe otra palabra o frase.");
-                String texto7b=sc.nextLine();
-                String textoConcatenado=concatenarTexto(texto7a, texto7b);
-                System.out.println("Los dos textos concatenados quedan asi: "+textoConcatenado);
-                break;
-
-                case 8:
-                System.out.println("Escribe una palabra o frase.");
-                String texto8=sc.nextLine();
-                System.out.println("Escribe una vocal.");
-                String vocal=sc.nextLine();
-                cambiarVocales(texto8, vocal);
-                break;
-
-                case 9:
-                System.out.println("Escribe una palabra o frase.");
-                String texto9=sc.nextLine();
-                codigoCaracteres(texto9);
-                break;
-
-                case 0:
-                System.out.println("Vale! hasta la proxima");
-                break;
-
-                default:
-                System.out.println("ERROR: esa opcion no existe.");
-            }
-            System.out.println("");
-            sc=new Scanner(System.in);
-            System.out.println("Elige una nueva funcion(1-9) o sal del programa(0):");
-            numero=sc.nextInt();
-            sc.nextLine();
         }
     }
 }
